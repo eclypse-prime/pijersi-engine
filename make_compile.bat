adb62e71bf18fcd31dbaadcfb782ec79eeca7f3f
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2022-08-11 23:14:25 +0200

    feat: build batch files will create missing folder if they don't exist

--- make_compile.bat
+++ make_compile.bat
@@ -1,4 +1,5 @@
 @echo off
+if not exist "build" mkdir build
 cd src
 g++ -c -O3 -I../include board.cpp piece.cpp pijersi_engine_wrap.cxx
 g++ -shared -I../include board.o piece.o pijersi_engine_wrap.o -o ../build/PijersiEngine.dll
--- make_swig.bat
+++ make_swig.bat
@@ -1,2 +1,3 @@
 @echo off
+if not exist "wrap" mkdir wrap
 swig -csharp -c++ -Iinclude -namespace PijersiEngine -outdir wrap -o src/pijersi_engine_wrap.cxx pijersi_engine.i
