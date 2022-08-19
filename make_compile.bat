799c6f30ce24786416f3d4bc1ca8b93844e89532
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2022-08-20 00:34:47 +0200

    fix: added missing -fopenmp flag in linker

--- make_compile.bat
+++ make_compile.bat
@@ -2,4 +2,4 @@
 if not exist "build" mkdir build
 cd src
 g++ -c -O3 -fopenmp -I../include board.cpp piece.cpp pijersi_engine_wrap.cxx
-g++ -shared -I../include board.o piece.o pijersi_engine_wrap.o -o ../build/PijersiCore.dll
+g++ -fopenmp -shared -I../include board.o piece.o pijersi_engine_wrap.o -o ../build/PijersiCore.dll
