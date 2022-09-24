e70de523eec072967691d96476697cf5d09b318d
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2022-09-25 00:48:41 +0200

    refactor: change the output directory to facilitate wrapping for different languages

--- make_swig.bat
+++ make_swig.bat
@@ -1,3 +1,3 @@
 @echo off
-if not exist "wrap" mkdir wrap
-swig -csharp -c++ -Iinclude -namespace PijersiEngine -outdir wrap -o src/pijersi_engine_wrap.cxx pijersi_engine.i
+if not exist "wrap_csharp" mkdir wrap_csharp
+swig -csharp -c++ -Iinclude -namespace PijersiEngine -outdir wrap_csharp -o src/pijersi_engine_wrap.cxx pijersi_engine.i
