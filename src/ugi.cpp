fd15999da4b959201cfd684fc1585ffd98ee844b
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2023-01-30 00:29:27 +0100

    feat: started implementing ugi position command (not functional atm)

--- src/ugi.cpp
+++ src/ugi.cpp
@@ -44,6 +44,10 @@ int main(int argc, char** argv)
                 cout << "id author Eclypse-Prime" << endl;
                 cout << "ugiok" << endl;
             }
+            else if (command == "isready")
+            {
+                cout << "readyok" << endl;
+            }
             else if (command == "go")
             {
                 if (words.size() >= 3)
@@ -75,29 +79,46 @@ int main(int argc, char** argv)
                         time_point<steady_clock> finishTime = steady_clock::now() + milliseconds(durationMilliseconds);
                         if (durationMilliseconds >= 0)
                         {
-                            uint32_t move = 0x00FFFFFF;
+                            uint32_t move = NULL_MOVE;
                             int depth = 1;
                             while (steady_clock::now() < finishTime)
                             {
                                 uint64_t remainingTimeMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(finishTime - std::chrono::steady_clock::now()).count();
                                 auto start = steady_clock::now();
-                                uint32_t proposedMove = board.searchDepth(depth, true, remainingTimeMilliseconds);
-                                if (proposedMove != 0x00FFFFFF)
+                                uint32_t proposedMove = board.searchDepth(depth, true, move, remainingTimeMilliseconds, false);
+                                auto end = steady_clock::now();
+                                if (proposedMove != NULL_MOVE)
                                 {
                                     move = proposedMove;
                                     string moveString = Logic::moveToString(move, board.getState());
-                                    auto end = steady_clock::now();
                                     int duration = duration_cast<milliseconds>(end - start).count();
                                     cout << "info depth " << depth << " seldepth " << depth << " pv " << moveString << endl;
                                     depth += 1;
                                 }
                             }
-                            if (move != 0x00FFFFFF)
+                            if (move != NULL_MOVE)
                             {
                                 string moveString = Logic::moveToString(move, board.getState());
                                 cout << "bestmove " << moveString << endl;
                                 board.playManual(move);
                             }
+                        }
+                    }
+                }
+            }
+            else if (command == "position")
+            {
+                if (words.size() >= 2)
+                {
+                    string mode = words[1];
+                    if (words.size() >= 3)
+                    {
+                        string parameter = words[2];
+                        
+                        if (mode == "fen")
+                        {
+                            // TODO: need to update stringstate for ugi
+                            board.setStringState(parameter);
                         }
                     }
                 }
