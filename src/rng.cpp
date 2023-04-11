fb6d45040604e6da19116cc0d6c8238b8975f915
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2023-04-11 09:05:20 +0200

    refactor: changed random distribution to positive only in PijersiEngine::RNG

--- src/rng.cpp
+++ src/rng.cpp
@@ -4,5 +4,5 @@ namespace PijersiEngine::RNG
 {
     std::random_device rd; // Obtaining random number from hardware
     std::mt19937 gen(rd()); // Seeding generator
-    std::uniform_real_distribution<float> distribution(-0.25f, 0.25f); // Defining uniform distribution
+    std::uniform_real_distribution<float> distribution(0.0f, 0.25f); // Defining uniform distribution
 }
