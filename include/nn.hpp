ad706ac0eb8eef451061edef5127f3f2f7ee07f6
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2023-04-11 09:06:40 +0200

    refactor: updated NN with Eigen for huge speedup and clarity

--- include/nn.hpp
+++ include/nn.hpp
@@ -1,37 +1,141 @@
 #ifndef NN_HPP
 #define NN_HPP
 #include <cstdint>
+#include <vector>
+#include <array>
 
-#define INPUT_TENSOR_SIZE 45*16
+#include <Eigen/Dense>
+
+#define N_INPUTS 720
+#define N_OUTPUTS_1 256
+#define N_OUTPUTS_2 32
+#define N_OUTPUTS_3 32
+#define N_OUTPUTS_4 1
+
+typedef Eigen::Matrix<float, N_INPUTS, 1> input_t;
+
+typedef Eigen::Matrix<float, N_OUTPUTS_1, 1> output1_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_2, 1> output2_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_3, 1> output3_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_4, 1> output4_t;
+
+typedef Eigen::MatrixXf weights_t;
+// typedef Eigen::Matrix<float, N_OUTPUTS_1, N_INPUTS> weights1_t;
+// typedef Eigen::Matrix<float, N_OUTPUTS_2, N_OUTPUTS_1> weights2_t;
+// typedef Eigen::Matrix<float, N_OUTPUTS_3, N_OUTPUTS_2> weights3_t;
+// typedef Eigen::Matrix<float, N_OUTPUTS_4, N_OUTPUTS_3> weights4_t;
+
+typedef Eigen::Matrix<float, N_OUTPUTS_1, 1> bias1_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_2, 1> bias2_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_3, 1> bias3_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_4, 1> bias4_t;
+
+typedef Eigen::Matrix<float, N_INPUTS, Eigen::Dynamic> inputs_trainer_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_1, Eigen::Dynamic> outputs_trainer1_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_2, Eigen::Dynamic> outputs_trainer2_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_3, Eigen::Dynamic> outputs_trainer3_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_4, Eigen::Dynamic> outputs_trainer4_t;
+
+typedef Eigen::Matrix<float, N_OUTPUTS_1, Eigen::Dynamic> errors_trainer1_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_2, Eigen::Dynamic> errors_trainer2_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_3, Eigen::Dynamic> errors_trainer3_t;
+typedef Eigen::Matrix<float, N_OUTPUTS_4, Eigen::Dynamic> errors_trainer4_t;
 
 namespace PijersiEngine::NN
 {
-    int8_t *cellsToTensor(uint8_t cells[45]);
-    int8_t _relu(int8_t input);
+    input_t cellsToInput(uint8_t cells[45], uint8_t currentPlayer);
+    float _sigmoid(float input);
+    float _dSigmoid(float input);
+    float _leakyRelu(float input);
+    float _dLeakyRelu(float input);
+
+    // struct Layer
+    // {
+    //     float *weights = nullptr;
+    //     float *biases = nullptr;
+    //     int inputSize = 0;
+    //     int outputSize = 0;
+    //     bool activation = false;
+
+    //     Layer(int newInputSize, int newOutputSize, bool newUseActivation);
+    //     void load();
+    //     template <typename T>
+    //     void forward(T *input, float *output);
+    //     void update(float learningRate, float* weightError, float* biasError);
+    //     ~Layer();
+    // };
+
+    // struct DenseN_INPUTSx256 : Layer
+    // {
+    //     DenseN_INPUTSx256();
+    // };
+
+    // struct Dense256x32 : Layer
+    // {
+    //     Dense256x32();
+    // };
+
+    // struct Dense32x32 : Layer
+    // {
+    //     Dense32x32();
+    // };
+
+    // struct Dense32x1 : Layer
+    // {
+    //     Dense32x1();
+    // };
 
     struct Network
     {
-        Dense720 layer1[32];
-        Dense32 layer2[32];
-        Dense32 layer3;
+        weights_t weights1;
+        weights_t weights2;
+        weights_t weights3;
+        weights_t weights4;
 
-        int16_t forward(int8_t input[INPUT_TENSOR_SIZE]);
+        bias1_t bias1;
+        bias2_t bias2;
+        bias3_t bias3;
+        bias4_t bias4;
+
+        // TODO: Implement incremental updates when possible
+        // void setInput(uint8_t cells[45], uint8_t currentPlayer);
+        Network();
+        void init();
+        float forward(uint8_t cells[45], uint8_t currentPlayer);
     };
 
-    struct Dense720
+    struct Trainer
     {
-        int8_t weights[INPUT_TENSOR_SIZE];
-        int8_t bias;
+        int batchSize = 1;
 
-        int8_t forward(int8_t input[INPUT_TENSOR_SIZE]);
-    };
+        weights_t weights1;
+        weights_t weights2;
+        weights_t weights3;
+        weights_t weights4;
 
-    struct Dense32
-    {
-        int8_t weights[32];
-        int8_t bias;
+        bias1_t bias1;
+        bias2_t bias2;
+        bias3_t bias3;
+        bias4_t bias4;
 
-        int8_t forward(int8_t input[32]);
+        inputs_trainer_t inputs;
+        outputs_trainer1_t outputs1;
+        outputs_trainer2_t outputs2;
+        outputs_trainer3_t outputs3;
+        outputs_trainer4_t outputs4;
+
+        errors_trainer1_t errors1;
+        errors_trainer2_t errors2;
+        errors_trainer3_t errors3;
+        errors_trainer4_t errors4;
+        errors_trainer4_t targets;
+
+        Trainer(int newBatchSize);
+        void setInput(uint8_t cells[45], uint8_t currentPlayer, float target, int batchIndex);
+        void forward();
+        float loss();
+        void back(float learningRate);
+        // ~Trainer();
     };
 }
 
--- src/nn.cpp
+++ src/nn.cpp
@@ -1,110 +1,294 @@
+#include <algorithm>
 #include <cstdint>
+#include <vector>
+
+#include <iostream>
+#include <omp.h>
 
 #include <nn.hpp>
+#include <rng.hpp>
 
 namespace PijersiEngine::NN
 {
-    int8_t *cellsToTensor(uint8_t cells[45])
+    input_t cellsToInput(uint8_t cells[45], uint8_t currentPlayer)
     {
-        int8_t input[INPUT_TENSOR_SIZE];
-
-        for (int k = 0; k < 45; k++)
+        input_t input = input_t::Zero();
+        if (currentPlayer == 0)
         {
-            if (cells[k] != 0)
+            for (int k = 0; k < 45; k++)
             {
-                switch (cells[k] & 14)
+                if (cells[k] != 0)
                 {
-                case 0:
-                    input[k * 16] = 1;
-                    break;
-                case 4:
-                    input[k * 16 + 1] = 1;
-                    break;
-                case 8:
-                    input[k * 16 + 2] = 1;
-                    break;
-                case 12:
-                    input[k * 16 + 3] = 1;
-                    break;
-                case 2:
-                    input[k * 16 + 4] = 1;
-                    break;
-                case 6:
-                    input[k * 16 + 5] = 1;
-                    break;
-                case 10:
-                    input[k * 16 + 6] = 1;
-                    break;
-                case 14:
-                    input[k * 16 + 7] = 1;
-                    break;
-                }
-                if (cells[k] >= 16)
-                {
-                    switch (cells[k] & 224)
+                    switch (cells[k] & 14)
                     {
                     case 0:
-                        input[k * 16 + 8] = 1;
+                        input(k * 16) = 1;
                         break;
-                    case 64:
-                        input[k * 16 + 9] = 1;
+                    case 4:
+                        input(k * 16 + 1) = 1;
                         break;
-                    case 128:
-                        input[k * 16 + 10] = 1;
+                    case 8:
+                        input(k * 16 + 2) = 1;
                         break;
-                    case 192:
-                        input[k * 16 + 11] = 1;
+                    case 12:
+                        input(k * 16 + 3) = 1;
                         break;
-                    case 32:
-                        input[k * 16 + 12] = 1;
+                    case 2:
+                        input(k * 16 + 4) = 1;
                         break;
-                    case 96:
-                        input[k * 16 + 13] = 1;
+                    case 6:
+                        input(k * 16 + 5) = 1;
                         break;
-                    case 160:
-                        input[k * 16 + 14] = 1;
+                    case 10:
+                        input(k * 16 + 6) = 1;
                         break;
-                    case 224:
-                        input[k * 16 + 15] = 1;
+                    case 14:
+                        input(k * 16 + 7) = 1;
                         break;
+                    }
+                    if (cells[k] >= 16)
+                    {
+                        switch (cells[k] & 224)
+                        {
+                        case 0:
+                            input(k * 16 + 8) = 1;
+                            break;
+                        case 64:
+                            input(k * 16 + 9) = 1;
+                            break;
+                        case 128:
+                            input(k * 16 + 10) = 1;
+                            break;
+                        case 192:
+                            input(k * 16 + 11) = 1;
+                            break;
+                        case 32:
+                            input(k * 16 + 12) = 1;
+                            break;
+                        case 96:
+                            input(k * 16 + 13) = 1;
+                            break;
+                        case 160:
+                            input(k * 16 + 14) = 1;
+                            break;
+                        case 224:
+                            input(k * 16 + 15) = 1;
+                            break;
+                        }
                     }
                 }
             }
         }
-
+        else
+        {
+            for (int k = 0; k < 45; k++)
+            {
+                if (cells[44 - k] != 0)
+                {
+                    switch (cells[44 - k] & 14)
+                    {
+                    case 2:
+                        input(k * 16) = 1;
+                        break;
+                    case 6:
+                        input(k * 16 + 1) = 1;
+                        break;
+                    case 10:
+                        input(k * 16 + 2) = 1;
+                        break;
+                    case 14:
+                        input(k * 16 + 3) = 1;
+                        break;
+                    case 0:
+                        input(k * 16 + 4) = 1;
+                        break;
+                    case 4:
+                        input(k * 16 + 5) = 1;
+                        break;
+                    case 8:
+                        input(k * 16 + 6) = 1;
+                        break;
+                    case 12:
+                        input(k * 16 + 7) = 1;
+                        break;
+                    }
+                    if (cells[44 - k] >= 16)
+                    {
+                        switch (cells[k] & 224)
+                        {
+                        case 32:
+                            input(k * 16 + 8) = 1;
+                            break;
+                        case 96:
+                            input(k * 16 + 9) = 1;
+                            break;
+                        case 160:
+                            input(k * 16 + 10) = 1;
+                            break;
+                        case 224:
+                            input(k * 16 + 11) = 1;
+                            break;
+                        case 0:
+                            input(k * 16 + 12) = 1;
+                            break;
+                        case 64:
+                            input(k * 16 + 13) = 1;
+                            break;
+                        case 128:
+                            input(k * 16 + 14) = 1;
+                            break;
+                        case 192:
+                            input(k * 16 + 15) = 1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
         return input;
     }
 
-    inline int8_t _relu(int8_t input)
+    float _sigmoid(float input)
     {
-        if (input >= 0)
-        {
-            return input;
-        }
-        return 0;
+        return 1.f / (1.f + exp(-input));
     }
 
-    
-
-    int8_t Dense720::forward(int8_t input[INPUT_TENSOR_SIZE])
+    float _dSigmoid(float input)
     {
-        int8_t sum = 0;
-        for (int k = 0; k < INPUT_TENSOR_SIZE; k++)
-        {
-            sum += weights[k] * input[k];
-        }
-        sum += bias;
-        return _relu(sum);
+        return input * (1 - input);
     }
 
-    int8_t Dense32::forward(int8_t input[32])
+    float _leakyRelu(float input)
     {
-        int8_t sum = 0;
-        for (int k = 0; k < 32; k++)
-        {
-            sum += weights[k] * input[k];
-        }
-        sum += bias;
-        return _relu(sum);
+        return std::max(0.f, input);
+        // return std::max(0.01f * input, input);
+    }
+
+    float _dLeakyRelu(float input)
+    {
+        // return (input > 0) ? 1 : 0.01f;
+        return (input > 0) ? 1 : 0.f;
+    }
+
+    Network::Network()
+    {
+    }
+
+    void Network::init()
+    {
+        weights1 = weights_t::Random(N_OUTPUTS_1, N_INPUTS);
+        weights2 = weights_t::Random(N_OUTPUTS_2, N_OUTPUTS_1);
+        weights3 = weights_t::Random(N_OUTPUTS_3, N_OUTPUTS_2);
+        weights4 = weights_t::Random(N_OUTPUTS_4, N_OUTPUTS_3);
+
+        bias1 = bias1_t::Random();
+        bias2 = bias2_t::Random();
+        bias3 = bias3_t::Random();
+        bias4 = bias4_t::Random();
+    }
+
+    float Network::forward(uint8_t cells[45], uint8_t currentPlayer)
+    {
+        input_t input = cellsToInput(cells, currentPlayer);
+        output1_t output1 = weights1 * input + bias1;
+        output1 = output1.cwiseMax(0.0f);
+        output2_t output2 = weights2 * output1 + bias2;
+        output2 = output2.cwiseMax(0.0f);
+        output3_t output3 = weights3 * output2 + bias3;
+        output3 = output3.cwiseMax(0.0f);
+        output4_t output4 = weights4 * output3 + bias4;
+        return output4(0);
+    }
+
+    Trainer::Trainer(int newBatchSize)
+    {
+        batchSize = newBatchSize;
+
+        weights1 = weights_t::Random(N_OUTPUTS_1, N_INPUTS);
+        weights2 = weights_t::Random(N_OUTPUTS_2, N_OUTPUTS_1);
+        weights3 = weights_t::Random(N_OUTPUTS_3, N_OUTPUTS_2);
+        weights4 = weights_t::Random(N_OUTPUTS_4, N_OUTPUTS_3);
+
+        bias1 = bias1_t::Random();
+        bias2 = bias2_t::Random();
+        bias3 = bias3_t::Random();
+        bias4 = bias4_t::Random();
+
+        inputs = inputs_trainer_t::Zero(N_INPUTS, batchSize);
+        outputs1 = outputs_trainer1_t::Zero(N_OUTPUTS_1, batchSize);
+        outputs2 = outputs_trainer2_t::Zero(N_OUTPUTS_2, batchSize);
+        outputs3 = outputs_trainer3_t::Zero(N_OUTPUTS_3, batchSize);
+        outputs4 = outputs_trainer4_t::Zero(N_OUTPUTS_4, batchSize);
+
+        errors1 = errors_trainer1_t::Zero(N_OUTPUTS_1, batchSize);
+        errors2 = errors_trainer2_t::Zero(N_OUTPUTS_2, batchSize);
+        errors3 = errors_trainer3_t::Zero(N_OUTPUTS_3, batchSize);
+        errors4 = errors_trainer4_t::Zero(N_OUTPUTS_4, batchSize);
+        targets = errors_trainer4_t::Zero(N_OUTPUTS_4, batchSize);
+    }
+
+    void Trainer::setInput(uint8_t cells[45], uint8_t currentPlayer, float target, int batchIndex)
+    {
+        inputs.col(batchIndex) = cellsToInput(cells, currentPlayer);
+        targets(batchIndex) = target;
+    }
+
+    void Trainer::forward()
+    {
+        outputs1 = (weights1 * inputs).colwise() + bias1;
+        outputs1 = outputs1.cwiseMax(0.0f);
+
+        outputs2 = (weights2 * outputs1).colwise() + bias2;
+        outputs2 = outputs2.cwiseMax(0.0f);
+
+        outputs3 = (weights3 * outputs2).colwise() + bias3;
+        outputs3 = outputs3.cwiseMax(0.0f);
+
+        outputs4 = (weights4 * outputs3).colwise() + bias4;
+    }
+
+    float Trainer::loss()
+    {
+        errors_trainer4_t errors4 = targets - outputs4;
+        return errors4.squaredNorm() / (float)batchSize;
+    }
+
+    void Trainer::back(float learningRate)
+    {
+        errors4 = outputs4 - targets;
+
+        errors3 = weights4.transpose() * errors4;
+        errors3 = errors3.cwiseProduct((errors_trainer3_t)(outputs3.array() > 0).cast<float>());
+
+        errors2 = weights3.transpose() * errors3;
+        errors2 = errors2.cwiseProduct((errors_trainer2_t)(outputs2.array() > 0).cast<float>());
+
+        errors1 = weights2.transpose() * errors2;
+        errors1 = errors1.cwiseProduct((errors_trainer1_t)(outputs1.array() > 0).cast<float>());
+
+        bias1_t biasError1 = errors1.rowwise().sum() * (2 / (float)batchSize);
+        bias2_t biasError2 = errors2.rowwise().sum() * (2 / (float)batchSize);
+        bias3_t biasError3 = errors3.rowwise().sum() * (2 / (float)batchSize);
+        bias4_t biasError4 = errors4.rowwise().sum() * (2 / (float)batchSize);
+
+        weights_t weightsError1 = errors1 * inputs.transpose();
+        weights_t weightsError2 = errors2 * outputs1.transpose();
+        weights_t weightsError3 = errors3 * outputs2.transpose();
+        weights_t weightsError4 = errors4 * outputs3.transpose();
+
+        weightsError1 *= (2 / (float)batchSize);
+        weightsError2 *= (2 / (float)batchSize);
+        weightsError3 *= (2 / (float)batchSize);
+        weightsError4 *= (2 / (float)batchSize);
+
+        bias1 -= learningRate * biasError1;
+        bias2 -= learningRate * biasError2;
+        bias3 -= learningRate * biasError3;
+        bias4 -= learningRate * biasError4;
+
+        weights1 -= learningRate * weightsError1;
+        weights2 -= learningRate * weightsError2;
+        weights3 -= learningRate * weightsError3;
+        weights4 -= learningRate * weightsError4;
     }
 }
