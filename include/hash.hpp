d958e9602b06de4d8b3a656865bb5c187c784cfe
Arthur Liu <arthur.liu.lsh@protonmail.com>
2023-09-16 00:43:45 +0200

    feat: implement LRU cache for transposition table

--- include/hash.hpp
+++ include/hash.hpp
@@ -1,11 +1,55 @@
 #ifndef HASH_HPP
 #define HASH_HPP
 
+#include <list>
+#include <unordered_map>
+#include <assert.h>
+
 namespace PijersiEngine::Hash
 {
-    extern uint64_t hashKeys[1530];
+    extern uint64_t pieceHashKeys[1575];
     void hashInit();
-    uint64_t hash(uint8_t cells[45]);
+    uint64_t hash(uint8_t cells[45], int recursionDepth);
+
+    template <class KEY_T, class VAL_T> class LRUCache{
+    private:
+        std::list<std::pair<KEY_T,VAL_T> > item_list;
+        std::unordered_map<KEY_T, decltype(item_list.begin()) > item_map;
+        size_t cache_size;
+    private:
+        void clean(void){
+            while(item_map.size()>cache_size){
+                auto last_it = item_list.end(); last_it --;
+                item_map.erase(last_it->first);
+                item_list.pop_back();
+            }
+        };
+    public:
+        LRUCache(int cache_size_):cache_size(cache_size_){
+        };
+
+        void put(const KEY_T &key, const VAL_T &val){
+            auto it = item_map.find(key);
+            if(it != item_map.end()){
+                item_list.erase(it->second);
+                item_map.erase(it);
+            }
+            item_list.push_front(std::make_pair(key,val));
+            item_map.insert(std::make_pair(key, item_list.begin()));
+            clean();
+        };
+        bool exist(const KEY_T &key){
+            return (item_map.count(key)>0);
+        };
+        VAL_T get(const KEY_T &key){
+            assert(exist(key));
+            auto it = item_map.find(key);
+            item_list.splice(item_list.begin(), item_list, it->second);
+            return it->second->second;
+        };
+    };
+
+    extern LRUCache<uint64_t, uint64_t> hashTable;
 }
 
 #endif
--- src/hash.cpp
+++ src/hash.cpp
@@ -1,110 +1,47 @@
 #include <random>
 
 #include <hash.hpp>
+#include <lookup.hpp>
 #include <rng.hpp>
 
 namespace PijersiEngine::Hash
 {
     std::uniform_int_distribution<uint64_t> uint64rng(0ULL, UINT64_MAX);
 
-    uint64_t hashKeys[1530];
+    uint64_t pieceHashKeys[1575];
+    uint64_t depthHashKeys[20];
+    LRUCache<uint64_t, uint64_t> hashTable(1024*1024*1024);
 
     void hashInit()
     {
         for (int index = 0; index < 1530; index++)
         {
-            hashKeys[index] = uint64rng(RNG::gen);
+            pieceHashKeys[index] = uint64rng(RNG::gen);
+        }
+        for (int index = 1530; index < 1545; index++)
+        {
+            pieceHashKeys[index] = 0;
+        }
+        for (int index = 0; index < 20; index++)
+        {
+            depthHashKeys[index] = uint64rng(RNG::gen);
         }
     }
 
     uint64_t hashPiece(uint8_t piece, int index)
     {
-        switch (piece)
-        {
-        case 17:
-            return hashKeys[0 + index];
-        case 21:
-            return hashKeys[45 + index];
-        case 25:
-            return hashKeys[90 + index];
-        case 81:
-            return hashKeys[135 + index];
-        case 85:
-            return hashKeys[180 + index];
-        case 89:
-            return hashKeys[225 + index];
-        case 145:
-            return hashKeys[270 + index];
-        case 149:
-            return hashKeys[315 + index];
-        case 153:
-            return hashKeys[360 + index];
-        case 209:
-            return hashKeys[405 + index];
-        case 213:
-            return hashKeys[450 + index];
-        case 217:
-            return hashKeys[495 + index];
-        case 221:
-            return hashKeys[540 + index];
-        case 1:
-            return hashKeys[585 + index];
-        case 5:
-            return hashKeys[630 + index];
-        case 9:
-            return hashKeys[675 + index];
-        case 13:
-            return hashKeys[720 + index];
-        case 51:
-            return hashKeys[765 + index];
-        case 55:
-            return hashKeys[810 + index];
-        case 59:
-            return hashKeys[855 + index];
-        case 115:
-            return hashKeys[900 + index];
-        case 119:
-            return hashKeys[945 + index];
-        case 123:
-            return hashKeys[990 + index];
-        case 179:
-            return hashKeys[1035 + index];
-        case 183:
-            return hashKeys[1080 + index];
-        case 187:
-            return hashKeys[1125 + index];
-        case 243:
-            return hashKeys[1170 + index];
-        case 247:
-            return hashKeys[1215 + index];
-        case 251:
-            return hashKeys[1260 + index];
-        case 255:
-            return hashKeys[1305 + index];
-        case 3:
-            return hashKeys[1350 + index];
-        case 7:
-            return hashKeys[1395 + index];
-        case 11:
-            return hashKeys[1440 + index];
-        case 15:
-            return hashKeys[1485 + index];
-        default:
-            return 0;
-        }
+        return pieceHashKeys[Lookup::pieceToIndex[piece] * 45 + index];
     }
 
     // Implement incremental hashing
-    uint64_t hash(uint8_t cells[45])
+    uint64_t hash(uint8_t cells[45], int recursionDepth)
     {
         uint64_t result = 0;
         for (int index = 0; index < 45; index++)
         {
-            if (cells[index] != 0)
-            {
-                result ^= hashPiece(cells[index], index);
-            }
+            result ^= hashPiece(cells[index], index);
         }
+        result ^= depthHashKeys[recursionDepth];
         return result;
     }
 }
