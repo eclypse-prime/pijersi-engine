989cca17676692924e6b93adb7a21342fc8dcaed
Arthur Liu <arthur.liu.lsh@protonmail.com>
2023-11-02 15:47:41 +0100

    refactor: updated opening book with depth 7 first move

--- include/openings.hpp
+++ include/openings.hpp
@@ -11,7 +11,7 @@ using std::string;
 namespace PijersiEngine::Openings
 {
     unordered_map<string, uint32_t> book = {
-        {"s-p-r-s-p-r-/p-r-s-wwr-s-p-/6/7/6/P-S-R-WWS-R-P-/R-P-S-R-P-S- w 0 1", 1774883U},
+        {"s-p-r-s-p-r-/p-r-s-wwr-s-p-/6/7/6/P-S-R-WWS-R-P-/R-P-S-R-P-S- w 0 1", 1447203},
         {"s-p-r-s-p-r-/p-r-s-wwr-s-p-/6/7/P-5/1S-R-WWS-R-P-/R-P-S-R-P-S- b 1 1", 1312256},
         {"s-p-r-s-p-r-/p-r-s-wwr-s-p-/6/SP6/6/2R-WWS-R-P-/R-P-S-R-P-S- b 1 1", 1246976},
         {"s-p-r-s-p-r-/p-r-s-wwr-s-p-/6/2SP4/6/2R-WWS-R-P-/R-P-S-R-P-S- b 1 1", 1378048},
